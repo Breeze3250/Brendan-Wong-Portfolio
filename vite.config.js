1eb5e8e771c04e49332216ec89d34b392eb8604b
Breeze3250 <brendanwong2005@gmail.com>
2026-03-08 14:17:58 -0400

    Updated vite file to reflect repo name change

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Brendan-Wong-Portfolio/',
+  base: '/Portfolio/',
   plugins: [
     react(),
     tailwindcss(),
